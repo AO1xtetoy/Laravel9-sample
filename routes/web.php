af379cac5c47bdf80a2d6496cca5a32cd72494fb
EC2 Default User <ec2-user@ip-172-31-8-156.ap-northeast-1.compute.internal>
2022-06-13 16:01:52 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本のダッシュボード表示(books.blade.php)
+*/
+Route::get('/', function () {
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', [
+        'books' => $books
+    ]);
+    //return view('books',compact('books')); //も同じ意味
+});
+
+/**
+* 新「本」を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    //以下に登録処理を記述（Eloquentモデル）
+
+  // Eloquentモデル
+  $books = new Book;
+  $books->item_name = $request->item_name;
+  $books->item_number = '1';
+  $books->item_amount = '1000';
+  $books->published = '2017-03-07 00:00:00';
+  $books->save(); 
+  return redirect('/');
+});
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    $book->delete();
+    return redirect('/');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
